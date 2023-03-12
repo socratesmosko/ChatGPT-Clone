a39ba3937d6cc1dcbcb4e482b0f6569357f15a04
John Andreopoulos <39243722+johnandreopoulos@users.noreply.github.com>
2023-03-12 14:47:57 +0200

    Improve code quality for AI server
    - Cleaned up the code for starting the AI server to make it more professional and easier to read.
    - Moved some code out of the `startServer` function to reduce duplication and improve organization.
    - Used better naming conventions and added comments to explain some of the options.
    - Added a default value for the `PORT` environment variable.

--- server/server.js
+++ server/server.js
@@ -4,51 +4,44 @@ import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 import path from 'path';
 
-export function startServer() {
-  dotenv.config();
+dotenv.config();
 
-  const configuration = new Configuration({
-    apiKey: "process.env.OPENAI_API_KEY",
-  });
+const app = express();
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAIApi(configuration);
 
-  const openai = new OpenAIApi(configuration);
+app.use(cors());
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use('/public', express.static(path.join(__dirname, '../client')));
 
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  app.set('view engine', 'ejs');
-  app.set('views', '../client');
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
 
-  const staticDir = path.resolve(`${process.cwd()}${path.sep}..${path.sep}client`);
-  app.use('/public', express.static(staticDir));
+app.post('/', async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-  app.get('/', async (req, res) => {
-    res.render('index.ejs');
-  });
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      maxTokens: 3000,
+      topP: 1,
+      frequencyPenalty: 0.5,
+      presencePenalty: 0,
+    });
 
-  app.post('/', async (req, res) => {
-    try {
-      const prompt = req.body.prompt;
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
+});
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0, // Higher values means the model will take more risks.
-        max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-        top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-        frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-        presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-      });
-
-      res.status(200).send({
-        bot: response.data.choices[0].text,
-      });
-
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(error || 'Something went wrong');
-    }
-  });
-
-  app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
-}
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`AI server started on http://localhost:${port}`));
